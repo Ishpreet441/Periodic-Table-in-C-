a49fc7eab3b17c2cd9a3f985cde302652ca16fef
Ishpreet441 <ishpreet1221627@jmit.ac.in>
2024-07-20 14:56:27 +0530

    Initial commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,55 @@
+else if (i == 28 || i == 46 || i == 78 || i == 110)
+               {
+                    elem[i].group = 10;
+                    elem[i].block = 'd';
+               }
+               else if (i == 29 || i == 47 || i == 79 || i == 111)
+               {
+                    elem[i].group = 11;
+                    elem[i].block = 'd';
+               }
+               else if (i == 30 || i == 48 || i == 80 || i == 112)
+               {
+                    elem[i].group = 12;
+                    elem[i].block = 'd';
+               }
+               else if (i == 5 || i == 13 || i == 31 || i == 49 || i == 81 || i == 113)
+               {
+                    elem[i].group = 13;
+                    elem[i].block = 'p';
+               }
+               else if (i == 6 || i == 14 || i == 32 || i == 50 || i == 82 || i == 114)
+               {
+                    elem[i].group = 14;
+                    elem[i].block = 'p';
+               }
+               else if (i == 7 || i == 15 || i == 33 || i == 51 || i == 83 || i == 115)
+               {
+                    elem[i].group = 15;
+                    elem[i].block = 'p';
+               }
+               else if (i == 8 || i == 16 || i == 34 || i == 52 || i == 84 || i == 116)
+               {
+                    elem[i].group = 16;
+                    elem[i].block = 'p';
+               }
+               else if (i == 9 || i == 17 || i == 35 || i == 53 || i == 85 || i == 117)
+               {
+                    elem[i].group = 17;
+                    elem[i].block = 'p';
+               }
+               else if (i == 2 || i == 10 || i == 18 || i == 36 || i == 54 || i == 86 || i == 118)
+               {
+                    elem[i].group = 18;
+                    elem[i].block = 'p';
+               }
+          }
+          for (i = 57; i <= 71; ++i)
+          {
+               elem[i].block = 'f';
+          }
+          for (i = 89; i <= 103; ++i)
+          {
+               elem[i].block = 'f';
+          }
+     }
